6c4eaf619caba5c48a880d1ae6d84ae080842234
rolnuiqdev <truongquynh2525@gmail.com>
2024-10-03 23:10:04 +0700

    chore: lor

--- lor/lor.go
+++ lor/lor.go
@@ -1,5 +1,10 @@
 package lor
 
+import (
+	"encoding/json"
+	"os"
+)
+
 func Map[T any, R any](arr []T, f func(T) R) []R {
 	result := make([]R, len(arr), cap(arr))
 
@@ -28,4 +33,43 @@ func Reduce[T any, R any](arr []T, init R, f func(R, T) R) R {
 	}
 
 	return init
+}
+
+func WriteJsonFile[T any](t *T, path string) error {
+	b, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(path, b, 0644)
+}
+
+func ReadJsonFile[T any](path string) (*T, error) {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var t T
+	err = json.Unmarshal(b, &t)
+	if err != nil {
+		return nil, err
+	}
+
+	return &t, nil
+}
+
+func StructToStruct[T, U any](t *T) (*U, error) {
+	var u U
+	b, err := json.Marshal(t)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(b, &u)
+	if err != nil {
+		return nil, err
+	}
+
+	return &u, nil
 }
