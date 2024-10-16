fe75d5623983ab2ae9a35fadf07c8f720dded3f9
rolniuq <truongquynh2525@gmail.com>
2024-10-16 14:46:06 +0700

    chore: lor

--- lor/lor.go
+++ lor/lor.go
@@ -73,3 +73,23 @@ func StructToStruct[T, U any](t *T) (*U, error) {
 
 	return &u, nil
 }
+
+func MapStructs[T any, U []any](t T, arr U) (U, error) {
+	bt, err := json.Marshal(t)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make(U, 0)
+
+	for _, v := range arr {
+		err := json.Unmarshal(bt, &v)
+		if err != nil {
+			return nil, err
+		}
+
+		res = append(res, v)
+	}
+
+	return res, nil
+}
--- lor/lor_test.go
+++ lor/lor_test.go
@@ -61,3 +61,37 @@ func TestReduce(t *testing.T) {
 
 	require.True(t, sum == 95)
 }
+
+type a1 struct {
+	CampaignId string
+}
+
+type a2 struct {
+	CampaignId string
+	Name       string
+}
+
+type a3 struct {
+	CampaignId string
+	Name       string
+	Age        int
+}
+
+func TestMapStructs(t *testing.T) {
+	a1 := a1{CampaignId: "123"}
+
+	arr := []interface{}{a2{}, a3{}}
+
+	res, err := MapStructs(a1, arr)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.True(t, len(res) == 2)
+
+	a2V, err := StructToStruct[interface{}, a2](&res[0])
+	require.NoError(t, err)
+	require.True(t, a2V.CampaignId == "123")
+
+	a3V, err := StructToStruct[interface{}, a3](&res[1])
+	require.NoError(t, err)
+	require.True(t, a3V.CampaignId == "123")
+}
